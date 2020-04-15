f5ded87d86d2f9b48e2cfdb356da2b77e0ba0a47
Em Ji Madhu <emjimadhu@gmail.com>
2020-04-15 20:13:22 +0530

    feat(src): return custom message

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import { CLIEngine } from 'eslint';
 
 const formatter = ((results: CLIEngine.LintResult[]): string => {
-  return JSON.stringify(results, null, 2);
+  let totalErrorCount = 0;
+  let totalWarningCount = 0;
+
+  results.forEach((result: CLIEngine.LintResult) => {
+    const {
+      errorCount,
+      warningCount
+    } = result;
+
+    if (errorCount + warningCount === 0) {
+      return;
+    }
+
+    totalErrorCount = totalErrorCount + errorCount;
+    totalWarningCount = totalWarningCount + warningCount;
+  });
+
+  if (totalErrorCount + totalWarningCount === 0) {
+    return (
+      'DONE: No lint errors found!\'}\n'
+    );
+  } else {
+    return (
+      ''
+    );
+  }
 });
 
 export = formatter;
